b9c6511f501e8012473743a11d6f2ed25918c4ce
Pernilla Sterner <hey@pernillasterner.se>
2024-05-06 15:47:57 +0200

    New endpoints (#3)
    * Remove files unused files and add logic to search for topics by keyword
    
    * Add error handling
    
    * Create function for handling finding topics by keyword and remove topics as param. It´s global variable
    
    * Search for questions by level
    
    * Seperate functions and routes

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ import express from "express";
 import cors from "cors";
 import expressListEndpoints from "express-list-endpoints";
 import { topics } from "./data/topics.json";
+import {
+  extractParam,
+  extractTopicNames,
+  findTopicByName,
+  findSubTopics,
+  findTopicsByKeyword,
+  findQuestionsByLevel,
+} from "./functions/topicFunctions.js";
 
 // Defines the port the app will run on. Defaults to 8080, but can be overridden
 // when starting the server. Example command to overwrite PORT env variable value:
@@ -14,30 +22,7 @@ let app = express();
 app.use(cors());
 app.use(express.json());
 
-// ____________________ FUNCTIONS ____________________
-
-// Function to extract param
-const extractParam = (req, param) => {
-  return req.params[param];
-};
-
-// Function to extract all topic names
-const extractTopicNames = () => topics.map((topic) => topic.name);
-
-// Function to find a topic name
-const findTopicByName = (topics, topicName) => {
-  return topics.find((topic) => topic.name === topicName);
-};
-
-// Function to find topic with specified name
-const findSubTopics = (topics, topicName) => {
-  const topic = topics.find((topic) => topic.name === topicName);
-
-  // If topic is found then return it´s subtopic, if not return null
-  return topic ? topic.subtopics : null;
-};
-
-// ____________________ ROUTES ____________________
+// ================== ROUTES ==================
 
 // // Start defining your routes here
 app.route("/").get((req, res) => {
@@ -45,14 +30,27 @@ app.route("/").get((req, res) => {
   res.send(endpoints);
 });
 
-// Route to get all topics
+// Get all topics or search for topics using keyword
 app.get("/topics", (req, res) => {
-  res.json(topics);
+  // topics?topic=javascript
+  const querySearch = req.query.topic;
+  if (querySearch) {
+    const filterTopics = findTopicsByKeyword(topics, querySearch);
+
+    if (filterTopics.length > 0) {
+      res.json(filterTopics);
+    } else {
+      res.status(404).json({ error: "No topics found matching the query" });
+    }
+  } else {
+    // If no search is done display all topics
+    res.json(topics);
+  }
 });
 
 // Get all topic names
 app.get("/topics/name", (req, res) => {
-  const topicNames = extractTopicNames();
+  const topicNames = extractTopicNames(topics);
   res.json(topicNames);
 });
 
@@ -88,23 +86,31 @@ app.get("/topics/:name/subtopics", (req, res) => {
 // Get all questions related to a specific topic
 app.get("/topics/:name/questions", (req, res) => {
   const topicName = extractParam(req, "name");
-  // Passing topics and topics name as params
+  // Passing topic name as params to find topics by name
   const topic = findTopicByName(topics, topicName);
 
-  res.json(topic);
+  if (topic) {
+    res.json(topic);
+  } else {
+    res
+      .status(404)
+      .json({ error: "No questions related to a specific topic was found" });
+  }
 });
 
-// Search for topics or questions by keyword
-
 // Get all topics or questions with a specific difficulty level
+app.get("/topics/subtopics/:level/questions", (req, res) => {
+  const level = extractParam(req, "level");
+
+  const questions = findQuestionsByLevel(topics, level);
+  res.json(questions);
+});
 
 // Get all topics or questions sorted by difficulty
-
 // Get all topics with a specific tag
-
 // Get all questions with a specific tag
 
 // // Start the server
-app.listen(port, () => {
+TODO: app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
